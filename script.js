4eb8acc7bf71bd83de68c7487ae86718cae1389b
G-gold <111223856+G-gold@users.noreply.github.com>
2022-08-19 14:15:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,20 @@
 'use strict';
+
 const feet = document.getElementById('feet');
-const feet2 = document.getElementById('feet2');
 const inches = document.getElementById('inches');
-const meters = document.getElementById('meters');
 
 feet.addEventListener('input', function () {
-  let ft = this.value;
-  let inch = ft * 12;
-  inches.value = inch;
+  let ft = feet.value * 12;
+  inches.value = ft;
+  //   return { ft };
 });
 
 inches.addEventListener('input', function () {
-  let inch = this.value;
-  let ft = inch / 12;
-  if (!Number.isInteger(ft)) {
-    ft = ft.toFixed(4);
+  let inch = inches.value / 12;
+  //To convert non-integer result to four decimal places
+  if (!Number.isInteger(inch)) {
+    inch = inch.toFixed(4);
   }
-  feet.value = ft;
-});
-
-feet2.addEventListener('input', function () {
-  let ft1 = this.value;
-  let mt = ft1 * 0.3048;
-  if (!Number.isInteger(mt)) {
-    mt = mt.toFixed(4);
-  }
-  meters.value = mt;
-});
-
-meters.addEventListener('input', function () {
-  let mt = this.value;
-  let ft1 = mt / 0.3048;
-  if (!Number.isInteger(ft1)) {
-    ft1 = ft1.toFixed(4);
-  }
-  feet2.value = ft1;
+  feet.value = inch;
+  //   return inch;
 });
